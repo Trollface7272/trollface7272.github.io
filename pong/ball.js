52bafdca54be19b1892919a39a63e08faba552df
Trollface7272 <trollface7272@post.cz>
2019-12-16 14:59:15 +0100

    sdsd

---
+++ pong/ball.js
@@ -0,0 +1,7 @@
+class Ball {
+    constructor() {
+        this.x = 650
+        this.y = 400
+        this.width = 20
+    }
+}
--- pong/player.js
+++ pong/player.js
@@ -1,5 +1,9 @@
-class player {
-    constructor() {
-        
+class Player {
+
+    constructor(x) {
+        this.x = x
+        this.y = 400
+        this.height = 50
+        this.width = 15
     }
 }
